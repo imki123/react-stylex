71b0e6e76b5768598d19835d1f5681f02a7a7cda
Hoodie <hoodie@haechi.io>
2024-01-02 09:25:42 +0900

    chore: set vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import styleX from "vite-plugin-stylex";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), styleX()],
+  // dev 실행시 자동으로 브라우저를 열어줌
+  server: {
+    open: true,
+  },
+});
